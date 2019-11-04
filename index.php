82b0a508b71a6b3bb517456c7d1743732358769b
SandSide <37347875+SandSide@users.noreply.github.com>
2019-11-04 14:59:53 +0000

    Add files via upload

---
+++ index.php
@@ -0,0 +1,15 @@
+<html>
+<head>
+</head>
+<body>
+<?php
+	
+	//include "db.php";
+	//$query = "DROP TABLE *";
+	//$mysql->exec($query);
+	//include "createTables.php";
+	//include "displayCustomers.php";
+	
+?>
+</body>
+</html>
