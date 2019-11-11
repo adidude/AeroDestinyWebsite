d60b8a8714b2379ccdb8d1d37bab705108c45785
SandSide <37347875+SandSide@users.noreply.github.com>
2019-11-11 11:58:39 +0000

    Add files via upload

--- PHP/displayStaff.php
+++ PHP/displayStaff.php
@@ -1,9 +1,12 @@
 <?php
 	include 'db.php';
 	if( isset($_POST['submit']) ){
+		#"SELECT branch_name, staff_name, job_title, phone_number, annual_salary FROM staff
+		#						WHERE staff_name  LIKE ':Name%' and branch_name LIKE ':Branch%' and annual_salary > ':sal_min'  AND  annual_salary < sal_max")
 		
-		$stmt = $mysql->prepare("SELECT branch_name, firstname, job_title, phone_number, annual_salary FROM staff;
-								WHERE firstname = :Name AND job_title=:Job_Title AND annual_salary < :Max AND annual_salary > :Min");		
+		$stmt = $mysql->prepare("SELECT branch_name, staff_name, job_title, phone_number, annual_salary FROM staff
+		WHERE staff_name  LIKE 'Rob%' and branch_name like '%' and annual_salary > '*'  AND  annual_salary < 999999");		
+								
 		$stmt->bindParam(":Name", $name);
 		$stmt->bindParam(":Job_Title", $job);
 		$stmt->bindParam(":Branch", $branch);
@@ -18,18 +21,22 @@
 		
 		$required = array('name', 'job_title', 'branch', 'sal_min', 'sal_max');
 
-		// Loop over field names, make sure each one exists and is not empty
-		$error = false;
-		foreach($required as $field) {
-			if (empty($_POST[$field])) {
-				$field = "*";
-			}
+
+		if (empty($_POST['sal_max'])){
+			$sal_max= 999999;
 		}
+
 		
+		echo $name. "<br>";
+		echo $job. "<br>";
+		echo $branch. "<br>";
+		echo $min. "<br>";
+		echo $max . "<br>";
 
 		$result = $stmt->fetchAll();
 		foreach( $result as $row ) {
-			echo "<br>";
+			echo "1";
+			echo $row['branch_name'];
 		}
 	}
 ?>
